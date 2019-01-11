6ada2bed7b6c50c00d19f5e4b792e53648c6c3a0
OdalisG <oxg101@aol.com>
2019-01-10 22:48:31 -0500

    Add existing file

--- contact-form.php
+++ contact-form.php
@@ -1,21 +1,36 @@
 <?php
+if(lisset($_POST['submit']))
+    if(!filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL))
+{
+    echo "error; please submit the form!";
+}
+$datetime = date("Y-m-d H:i:s");
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-
+//validation
+if(empty($name)||empty($visitor_email))
+{
+    echo "Name and email is mandatory!";
+    exit;
+}
+$email_from = 'admin@odalisgarcia502.com'
 $email_subject ="Form Submitted";
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-               "User Message: $message.\n";
+$email_body = "New mail from  
+     $name.\n".
+                "email address: $visitor_email.\n".
+               "User Message: \n $message";
 
-$to ="admin@odalisgarcia502.com";
+$to ='admin@odalisgarcia502.com';
 
-$headers = "From: $email_from \r\n";
-$headers .="Reply-To: $visitor_email \r\n";
+$header = "From: ".$ name . " $email " . "\r\n";
+$header .="Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
+ 
+        
 
-header("Location: contact.html");
+header("Location: index.html");
 
 ?>
